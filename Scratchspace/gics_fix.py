179432f8ca4fec3aa5169be03368162065fd12e4
Chris Nelson <dj.ccelson@gmail.com>
2024-08-31 21:37:03 -0500

    New version of gics modified

--- Scratchspace/gics_fix.py
+++ Scratchspace/gics_fix.py
@@ -2,16 +2,21 @@ import pandas as pd
 import json
 
 def gics_hydrate():
-    df = pd.read_csv('Gics_modified.csv')
+    df = pd.read_csv('Gics_modified_gpt4.csv')
     count = 0
     for index, row in df.iterrows():
         try:
-            row['climate_risk'] = row['climate_risk'].replace("'", "\"")
-            json.loads(str(row['climate_risk']))
+            # 
+            obj = json.loads(str(row['ClimateRiskMitigation']))
+            df.at[row.name,'ClimateRiskMitigation'] = str(obj)
+            row['ClimateRiskMitigation'] = row['ClimateRiskMitigation'].replace("'", "\"")
+            print(obj)
         except Exception as e: 
             count+=1
             print(e)
             print(row['SubIndustryDescription'])
     print(count)
+    print(df.head(10))
+    df.to_csv('Gics_modified_gpt4_v2.csv')
 
 gics_hydrate()
