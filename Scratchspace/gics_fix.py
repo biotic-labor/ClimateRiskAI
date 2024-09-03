8e8711401cb34968e2a730618f7a21deb319d9a4
Chris Nelson <dj.ccelson@gmail.com>
2024-09-03 09:48:59 -0500

    fix gics maybe

--- Scratchspace/gics_fix.py
+++ Scratchspace/gics_fix.py
@@ -9,7 +9,7 @@ def gics_hydrate():
             # 
             obj = json.loads(str(row['ClimateRiskMitigation']))
             row['ClimateRiskMitigation'] = str(obj).strip()
-            row['ClimateRiskMitigation'] = row['ClimateRiskMitigation'].replace("'", "\"")
+            # row['ClimateRiskMitigation'] = row['ClimateRiskMitigation'].replace("\"", "\'")
             if row['ClimateRiskMitigation'][-1] != ']':
                 print(row['ClimateRiskMitigation'])
                 print(row['ClimateRiskMitigation'].strip())
@@ -18,7 +18,7 @@ def gics_hydrate():
             # count+=1
             print(e)
     print(count)
-    # print(df.head(10))
-    df.to_csv('Gics_modified_gpt4_v2.csv')
+    print(df.head(10))
+    # df.to_csv('Gics_modified_gpt4_v2.csv')
 
 gics_hydrate()
