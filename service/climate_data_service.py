1232d028eb39898caed5377d1494c947a3f93c7b
Chris Nelson <dj.ccelson@gmail.com>
2024-12-14 10:27:21 -0600

    Change from beta to regular api

--- service/climate_data_service.py
+++ service/climate_data_service.py
@@ -19,7 +19,7 @@ def retrieve_climate_data(location: Location, query_type: QueryType) -> pd.DataF
         "precip": f"?query=annually(pr,sum)&from_date={start_date}T00%3A00%3A00.000Z&to_date={end_date}T00%3A00%3A00.000Z&latitude={latlong[0]}&longitude={latlong[1]}&apikey={api_key}",
         "dew":f"?query=annually(dew,mean)&from_date={start_date}T00%3A00%3A00.000Z&to_date={end_date}T00%3A00%3A00.000Z&latitude={latlong[0]}&longitude={latlong[1]}&apikey={api_key}"
         }
-    url = f'https://beta.climatedataservice.com/v6/series/csv{url_dictionary[query_type.name]}'
+    url = f'https://api.climatedataservice.com/v6/series/csv{url_dictionary[query_type.name]}'
     df = pd.read_csv(url, skiprows=12)
     df.drop(columns=["latitude", "longitude"], inplace=True, axis=1)
     return df
